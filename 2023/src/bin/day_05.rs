b7c79028f407633c982ec9964c8d6876f2f79d5e
Lawrence Bell <lawrence.matthew.bell@gmail.com>
2023-12-05 21:36:17 +0000

    do day 5, part 2

--- 2023/src/bin/day_05.rs
+++ 2023/src/bin/day_05.rs
@@ -1,15 +1,19 @@
+use gcollections::ops::*;
+use interval::interval_set::ToIntervalSet;
+use interval::ops::*;
+use interval::{Interval, IntervalSet};
 use nom::{bytes::complete as nom_byte, character::complete as nom_char};
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct Range {
-    dest_start: i64,
-    source_start: i64,
-    len: i64,
+#[derive(Debug, PartialEq, Eq)]
+struct IntervalMap {
+    dest: Interval<i64>,
+    source: Interval<i64>,
+    shift: i64,
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq)]
 struct Mapping<'a> {
-    ranges: Vec<Range>,
+    maps: Vec<IntervalMap>,
     from: &'a str,
     to: &'a str,
 }
@@ -21,26 +25,26 @@ fn parse_seeds(input: &str) -> nom::IResult<&str, Vec<i64>> {
     nom::multi::separated_list1(nom_char::space1, nom_char::i64)(input)
 }
 
-fn parse_range(input: &str) -> nom::IResult<&str, Range> {
+fn parse_map(input: &str) -> nom::IResult<&str, IntervalMap> {
     let (input, dest_start) = nom_char::i64(input)?;
     let input = nom_char::space1(input)?.0;
-    let (input, source_start) = nom_char::i64(input)?;
+    let (input, src_start) = nom_char::i64(input)?;
     let input = nom_char::space1(input)?.0;
     let (input, len) = nom_char::i64(input)?;
 
-    let range = Range {
-        dest_start,
-        source_start,
-        len,
+    let map = IntervalMap {
+        dest: Interval::new(dest_start, dest_start + len),
+        source: Interval::new(src_start, src_start + len),
+        shift: dest_start - src_start,
     };
-    Ok((input, range))
+    Ok((input, map))
 }
 
-fn parse_ranges(input: &str) -> nom::IResult<&str, Vec<Range>> {
-    nom::multi::separated_list1(nom_char::multispace1, parse_range)(input)
+fn parse_maps(input: &str) -> nom::IResult<&str, Vec<IntervalMap>> {
+    nom::multi::separated_list1(nom_char::multispace1, parse_map)(input)
 }
 
-fn parse_map(input: &str) -> nom::IResult<&str, Mapping> {
+fn parse_mapping(input: &str) -> nom::IResult<&str, Mapping> {
     let (input, from) = nom_char::alpha1(input)?;
     let input = nom_char::char('-')(input)?.0;
     let input = nom_byte::tag_no_case("to")(input)?.0;
@@ -50,50 +54,118 @@ fn parse_map(input: &str) -> nom::IResult<&str, Mapping> {
     let input = nom_byte::tag_no_case("map")(input)?.0;
     let input = nom_char::char(':')(input)?.0;
     let input = nom_char::multispace1(input)?.0;
-    let (input, ranges) = parse_ranges(input)?;
+    let (input, maps) = parse_maps(input)?;
 
-    let mapping = Mapping { ranges, from, to };
+    let mapping = Mapping { maps, from, to };
     Ok((input, mapping))
 }
 
-fn parse_maps(input: &str) -> nom::IResult<&str, Vec<Mapping>> {
-    nom::multi::separated_list1(nom_char::multispace1, parse_map)(input)
+fn parse_mappings(input: &str) -> nom::IResult<&str, Vec<Mapping>> {
+    nom::multi::separated_list1(nom_char::multispace1, parse_mapping)(input)
 }
 
 fn parse_input(input: &str) -> nom::IResult<&str, (Vec<i64>, Vec<Mapping>)> {
     let (input, seeds) = parse_seeds(input)?;
     let input = nom_char::multispace1(input)?.0;
-    let (input, maps) = parse_maps(input)?;
+    let (input, maps) = parse_mappings(input)?;
 
     Ok((input, (seeds, maps)))
 }
 
-fn run_map(seed: i64, map: &Mapping) -> i64 {
-    for range in &map.ranges {
-        let start = range.source_start;
-        let end = range.source_start + range.len;
-        if (start <= seed) && (seed < end) {
-            let shift = range.dest_start - range.source_start;
-            return seed + shift;
+fn run_map_at(seed: i64, mapping: &Mapping) -> i64 {
+    for map in &mapping.maps {
+        if (map.source.lower() <= seed) && (seed < map.source.upper()) {
+            return seed + map.shift;
         }
     }
     return seed;
 }
 
-fn run_maps(seed: i64, maps: &Vec<Mapping>) -> i64 {
+fn run_maps_at(seed: i64, mappings: &Vec<Mapping>) -> i64 {
     let mut value = seed;
-    for map in maps {
-        value = run_map(value, map);
+    for map in mappings {
+        value = run_map_at(value, map);
     }
     value
 }
 
-fn part_1(seeds: &Vec<i64>, maps: &Vec<Mapping>) -> i64 {
+fn part_1(seeds: &Vec<i64>, mappings: &Vec<Mapping>) -> i64 {
     seeds
         .iter()
-        .map(|seed| run_maps(*seed, maps))
+        .map(|seed| run_maps_at(*seed, mappings))
         .min()
         .expect("there is a minimal seed")
+}
+
+fn run_map_over(
+    interval: &Interval<i64>,
+    mapping: &Mapping,
+) -> IntervalSet<i64> {
+    let mut image = IntervalSet::empty();
+
+    // Add the image of the parts which _do_ overlap with source intervals.
+    for map in &mapping.maps {
+        let overlap = interval.intersection(&map.source);
+        if !overlap.is_empty() {
+            let shifted = overlap + map.shift;
+            image = image.union(&interval_into_set(&shifted));
+        }
+    }
+
+    // Add the image of the parts which _don't_ overlap with source intervals.
+    let source_sets = interval_maps_to_set(&mapping.maps);
+    let set = interval_into_set(interval);
+    image = image.union(&set.difference(&source_sets));
+
+    image
+}
+
+fn run_maps_over(
+    interval: &Interval<i64>,
+    mappings: &Vec<Mapping>,
+) -> IntervalSet<i64> {
+    let mut image = interval_into_set(interval);
+
+    for mapping in mappings {
+        image = image
+            .iter()
+            .map(|int| run_map_over(int, mapping))
+            .fold(IntervalSet::empty(), |a, b| a.union(&b));
+    }
+
+    image
+}
+
+fn interval_maps_to_set(maps: &Vec<IntervalMap>) -> IntervalSet<i64> {
+    let mut pairs = maps
+        .iter()
+        .map(|map| (map.source.lower(), map.source.upper()))
+        .collect::<Vec<_>>();
+
+    // This sort is necessary to avoid a panic - a bug in the library?
+    pairs.sort();
+    pairs.to_interval_set()
+}
+
+fn interval_into_set(interval: &Interval<i64>) -> IntervalSet<i64> {
+    (interval.lower(), interval.upper()).to_interval_set()
+}
+
+fn part_2(seeds: &Vec<i64>, mappings: &Vec<Mapping>) -> i64 {
+    let mut results = Vec::new();
+
+    for i in (0..seeds.len()).step_by(2) {
+        let start = seeds[i];
+        let len = seeds[i + 1];
+        let interval = Interval::new(start, start + len);
+        results.push(run_maps_over(&interval, mappings));
+    }
+
+    results
+        .iter()
+        .map(IntervalSet::lower)
+        .min()
+        .expect("non-zero amount of seeds")
 }
 
 fn main() {
@@ -101,4 +173,5 @@ fn main() {
     let (seeds, maps) = parse_input(input).expect("can parse input").1;
 
     println!("{}", part_1(&seeds, &maps));
+    println!("{}", part_2(&seeds, &maps));
 }
