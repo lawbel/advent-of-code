80083290cbfe5450eeb4aaf74d2e72d71d417b32
Lawrence Bell <lawrence.matthew.bell@gmail.com>
2023-12-09 15:57:07 +0000

    do day 9, part 2

--- 2023/src/bin/day_09.rs
+++ 2023/src/bin/day_09.rs
@@ -11,8 +11,12 @@ fn parse_lists(input: &str) -> nom::IResult<&str, Vec<Vec<i32>>> {
     Ok((input, lists))
 }
 
-fn to_deltas(vals: &Vec<i32>) -> Vec<i32> {
+fn forward_deltas(vals: &Vec<i32>) -> Vec<i32> {
     vals.windows(2).map(|w| w[1] - w[0]).collect()
+}
+
+fn backward_deltas(vals: &Vec<i32>) -> Vec<i32> {
+    vals.windows(2).map(|w| w[0] - w[1]).collect()
 }
 
 fn predict_next(vals: &Vec<i32>) -> i32 {
@@ -21,7 +25,19 @@ fn predict_next(vals: &Vec<i32>) -> i32 {
 
     while !deltas.iter().all(|&x| x == 0) {
         result += deltas[deltas.len() - 1];
-        deltas = to_deltas(&deltas);
+        deltas = forward_deltas(&deltas);
+    }
+
+    result
+}
+
+fn predict_prev(vals: &Vec<i32>) -> i32 {
+    let mut deltas = vals.clone();
+    let mut result = 0;
+
+    while !deltas.iter().all(|&x| x == 0) {
+        result += deltas[0];
+        deltas = backward_deltas(&deltas);
     }
 
     result
@@ -31,9 +47,14 @@ fn part_1(lists: &Vec<Vec<i32>>) -> i32 {
     lists.iter().map(predict_next).sum()
 }
 
+fn part_2(lists: &Vec<Vec<i32>>) -> i32 {
+    lists.iter().map(predict_prev).sum()
+}
+
 fn main() {
     let input = include_str!("../../inputs/day_09.txt");
     let lists = parse_lists(&input).expect("can parse input").1;
 
     println!("{}", part_1(&lists));
+    println!("{}", part_2(&lists));
 }
