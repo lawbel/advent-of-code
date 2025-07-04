e0473bb10fffae19a2cdb67727ea2048d81d8d0e
Lawrence Bell <lawbel@hotmail.co.uk>
2025-07-04 15:20:07 +0100

    update flake.nix
    We have switched over to a different zig flake input that is supposed
    to handle the wrapping of `/usr/bin/env` hardcoded into zig during the
    build process.
    
    Besides that, we have added in convenience outputs that can be used
    like `nix run .#day-01` - this will compile day 1 in ReleaseSmall mode
    (should be quicker than debug), point it at the input file via
    environment variables, and then run it. It doesn't benefit from
    caching so it's always slow, but it is nice to have the option.

--- 2024/flake.nix
+++ 2024/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    zig.url = "github:mitchellh/zig-overlay";
+    zig.url = "github:cloudef/zig2nix";
     zls.url = "github:zigtools/zls";
   };
 
@@ -12,23 +12,43 @@
       system = "x86_64-linux";
       name = "advent_2024";
       pkgs = import nixpkgs { inherit system; };
-      zig-exe = zig.packages.${system}."0.14.0";
+      zig-exe = zig.packages.${system}."zig-0_14_0";
       zls-exe = zls.packages.${system}.zls;
 
+      inherit (pkgs.lib) attrsets lists strings;
+      dayNums = lists.range 1 7;
+      dayStrs = map (strings.fixedWidthNumber 2) dayNums;
+      rename = name: value: attrsets.nameValuePair "day-${name}" value;
+      daysNumbered = target: attrsets.genAttrs dayStrs (runDay target);
+      daysNamed = target: attrsets.mapAttrs' rename (daysNumbered target);
+      runDay = target: num: pkgs.writeShellApplication {
+        name = "day-${num}";
+        runtimeInputs = [ zig-exe ];
+        text = ''
+          export ZIG_AOC_DAY_${num}=${./txt/day_${num}.txt}
+          zig build day-${num} \
+            -Doptimize=ReleaseSmall \
+            -Dtarget=${target} \
+            --color off
+        '';
+      };
+
     in {
-      packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
-        inherit name;
-        src = ./.;
-        nativeBuildInputs = [ zig-exe ];
-        buildPhase = ''
-          zig build install \
-            --global-cache-dir .cache \
-            --prefix $out
-        '';
-        doCheck = false;
-        checkPhase = ''
-          zig build test --global-cache-dir .cache
-        '';
+      packages.${system} = daysNamed system // {
+        default = self.packages.${system}.compile;
+        compile = pkgs.stdenvNoCC.mkDerivation {
+          inherit name;
+          src = ./.;
+          nativeBuildInputs = [ zig-exe ];
+          buildPhase = ''
+            export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+            zig build install \
+              -Doptimize=ReleaseFast \
+              -Dtarget=${system} \
+              --color off \
+              --prefix $out
+          '';
+        };
       };
 
       devShells.${system}.default = pkgs.mkShell {
