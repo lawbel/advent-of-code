f7f8439d0ae38f32c66fced156ba2e40c00f2818
Lawrence Bell <lawbel@hotmail.co.uk>
2025-07-05 10:27:46 +0100

    reformat flake.nix with alejandra

--- 2024/flake.nix
+++ 2024/flake.nix
@@ -7,67 +7,70 @@
     zls.url = "github:zigtools/zls";
   };
 
-  outputs = { self, nixpkgs, zig, zls, ... }:
-    let
-      system = "x86_64-linux";
-      name = "advent_2024";
-      pkgs = import nixpkgs { inherit system; };
-      zig-exe = zig.packages.${system}."zig-0_14_0";
-      zls-exe = zls.packages.${system}.zls;
+  outputs = {
+    nixpkgs,
+    zig,
+    zls,
+    ...
+  }: let
+    system = "x86_64-linux";
+    name = "advent_2024";
+    pkgs = import nixpkgs {inherit system;};
+    lib = pkgs.lib;
+    zig-exe = zig.packages.${system}."zig-0_14_0";
+    zls-exe = zls.packages.${system}.zls;
 
-      inherit (pkgs.lib) attrsets lists strings;
-      dayNums = lists.range 1 8;
-      dayStrs = map (strings.fixedWidthNumber 2) dayNums;
+    dayNums = builtins.genList (i: i + 1) 8;
+    dayStrs = builtins.map (lib.strings.fixedWidthNumber 2) dayNums;
 
-      mkShellApp = sys: name: build: exports: {
-        type = "app";
-        program = "${mkShellProg sys name build exports}/bin/${name}";
-      };
-      mkShellProg = sys: name: build: exports: pkgs.writeShellApplication {
+    mkShellApp = sys: name: build: exports: {
+      type = "app";
+      program = "${mkShellProg sys name build exports}/bin/${name}";
+    };
+    mkShellProg = sys: name: build: exports:
+      pkgs.writeShellApplication {
         inherit name;
-        runtimeInputs = [ zig-exe ];
+        runtimeInputs = [zig-exe];
         text = ''
           ${exports}
           zig build ${build} \
-            -Doptimize=ReleaseSmall \
+            -Doptimize=ReleaseFast \
             -Dtarget=${sys} \
             --color off
         '';
       };
 
-      export = num: "export ZIG_AOC_DAY_${num}=${./txt/day_${num}.txt}";
-      rename = name: value: attrsets.nameValuePair "day-${name}" value;
+    export = num: "export ZIG_AOC_DAY_${num}=${./txt/day_${num}.txt}";
+    rename = name: value: lib.attrsets.nameValuePair "day-${name}" value;
 
-      dayApps = sys: attrsets.mapAttrs' rename (days sys);
-      days = sys: attrsets.genAttrs dayStrs (day sys);
-      day = sys: num: mkShellApp sys "day-${num}" "day-${num}" (export num);
+    dayApps = sys: lib.attrsets.mapAttrs' rename (days sys);
+    days = sys: lib.attrsets.genAttrs dayStrs (day sys);
+    day = sys: num: mkShellApp sys "day-${num}" "day-${num}" (export num);
+  in {
+    packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
+      inherit name;
+      src = ./.;
+      nativeBuildInputs = [zig-exe];
+      buildPhase = ''
+        export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+        zig build install \
+          -Doptimize=ReleaseFast \
+          -Dtarget=${system} \
+          --color off \
+          --prefix $out
+      '';
+    };
 
-    in {
-      packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
-        inherit name;
-        src = ./.;
-        nativeBuildInputs = [ zig-exe ];
-        buildPhase = ''
-          export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
-          zig build install \
-            -Doptimize=ReleaseFast \
-            -Dtarget=${system} \
-            --color off \
-            --prefix $out
-        '';
-      };
-
-      apps.${system} = dayApps system // {
+    apps.${system} =
+      dayApps system
+      // {
         all-days = mkShellApp system "all-days" "all" (
-          strings.concatMapStringsSep "\n" export dayStrs
+          lib.strings.concatMapStringsSep "\n" export dayStrs
         );
       };
 
-      devShells.${system}.default = pkgs.mkShell {
-        nativeBuildInputs = [
-          zig-exe
-          zls-exe
-        ];
-      };
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = [zig-exe zls-exe];
     };
+  };
 }
