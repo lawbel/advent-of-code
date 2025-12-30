fced86e8e381ecc7e942e1bd4527f4e576711b51
Lawrence Bell <lawbel@hotmail.co.uk>
2025-12-30 03:11:53 +0000

    finish 2025, day 4

--- 2025/src/day_04.zig
+++ 2025/src/day_04.zig
@@ -10,21 +10,64 @@ pub fn main() !void {
     try runDay(
         .{ .day = 4 },
         .{ part1, Alloc, Input },
-        .{ part2, Input },
+        .{ part2, Alloc, Input },
     );
 }
 
 /// Day 4, part 1 - how many rolls of paper can be accessed by a forklift?
-pub fn part1(alloc: std.mem.Allocator, input: []const u8) !u32 {
-    var accessible: u32 = 0;
-    var grid: Grid = try .parse(alloc, input);
+pub fn part1(alloc: std.mem.Allocator, input: []const u8) !usize {
+    var grid = try Grid.parse(alloc, input);
+    defer grid.deinit(alloc);
+    var points = try accessible(alloc, grid);
+    defer points.deinit(alloc);
+
+    return points.items.len;
+}
+
+test part1 {
+    const alloc = std.testing.allocator;
+    try std.testing.expectEqual(13, part1(alloc, example));
+}
+
+/// Day 4, part 2 - how many rolls of paper in total can be removed by the
+/// Elves and their forklifts?
+pub fn part2(alloc: std.mem.Allocator, input: []const u8) !usize {
+    var grid = try Grid.parse(alloc, input);
     defer grid.deinit(alloc);
 
-    const roll = '@';
-    const limit = 4;
+    var removed: usize = 0;
+    while (true) {
+        var list = try accessible(alloc, grid);
+        defer list.deinit(alloc);
+
+        if (list.items.len == 0) break;
+
+        removed += list.items.len;
+        for (list.items) |pt| {
+            grid._0.items[pt.y].items[pt.x] = empty;
+        }
+    }
+
+    return removed;
+}
+
+test part2 {
+    const alloc = std.testing.allocator;
+    try std.testing.expectEqual(43, part2(alloc, example));
+}
+
+/// Which rolls of paper are accessible - adjacent tiles occupied with another
+/// roll of paper is less than `limit`? Caller owns returned memory.
+pub fn accessible(
+    alloc: std.mem.Allocator,
+    grid: Grid,
+) !std.ArrayListUnmanaged(Point) {
+    var list: std.ArrayListUnmanaged(Point) = .empty;
+    errdefer list.deinit(alloc);
+
     const height = grid._0.items.len;
     const width = grid._0.items[0].items.len;
-    const adjacent = [8]struct { x: isize, y: isize }{
+    const adjacent = [8]struct { x: i8, y: i8 }{
         .{ .x = -1, .y = -1 }, .{ .x = 0, .y = -1 },
         .{ .x = 1, .y = -1 },  .{ .x = -1, .y = 0 },
         .{ .x = 1, .y = 0 },   .{ .x = -1, .y = 1 },
@@ -37,7 +80,7 @@ pub fn part1(alloc: std.mem.Allocator, input: []const u8) !u32 {
 
             var rolls: usize = 0;
             for (adjacent) |off| {
-                const nbr = .{
+                const nbr: Point = .{
                     .x = switch (off.x) {
                         1 => if (x + 1 < width) x + 1 else continue,
                         -1 => if (x > 0) x - 1 else continue,
@@ -49,29 +92,33 @@ pub fn part1(alloc: std.mem.Allocator, input: []const u8) !u32 {
                         else => y,
                     },
                 };
+
                 if (grid._0.items[nbr.y].items[nbr.x] == roll) {
                     rolls += 1;
                 }
             }
 
-            if (rolls < limit) accessible += 1;
+            if (rolls < limit) {
+                const pos: Point = .{ .x = x, .y = y };
+                try list.append(alloc, pos);
+            }
         }
     }
 
-    return accessible;
+    return list;
 }
 
-test part1 {
-    const alloc = std.testing.allocator;
-    const accessible = try part1(alloc, example);
-    try std.testing.expectEqual(13, accessible);
-}
+/// The ASCII character representing a roll of paper.
+const roll: u8 = '@';
 
-/// Day 4, part 2 - [...]
-pub fn part2(input: []const u8) !u64 {
-    _ = input;
-    return 0;
-}
+/// An ASCII character representing any empty tile.
+const empty: u8 = '.';
+
+/// Below this number, rolls of paper are accessible.
+const limit = 4;
+
+/// A coordinate point `(x, y)`.
+const Point = struct { x: usize, y: usize };
 
 /// A rectangular grid of characters (`u8`s).
 const Grid = struct {
